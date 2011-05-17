29154b5cec619e404d596c0b696c2dc7b56167f7
Eric Oestrich <eric@oestrich.org>
2011-05-16 20:01:21 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+
+gem 'pg'
+
+gem 'jquery-rails'
+gem 'haml'
+gem 'haml-rails'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Listmaker::Application.config.secret_token = 'caade906efdc549430854a2d02288b0b2d032d168ffd2ad980af63693918435b624673a04d46776c33ae457e5e1bc4a8845e46e6b53759d0ec5585b2e6d036b8'
