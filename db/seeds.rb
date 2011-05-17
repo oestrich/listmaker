39903ee8f757ac708130ba9f48773363cadc418a
Eric Oestrich <eric@oestrich.org>
2011-05-16 22:35:00 -0400

    troop type seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Factory(:troop_type, :name => "Lord", :percentage => 25)
+Factory(:troop_type, :name => "Heroes", :percentage => 25)
+Factory(:troop_type, :name => "Core", :percentage => 50)
+Factory(:troop_type, :name => "Special", :percentage => 75)
+Factory(:troop_type, :name => "Rare", :percentage => 25)
