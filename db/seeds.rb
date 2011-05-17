5d3ffc49544c52f9321ce6ff87f1375ec3710628
Eric Oestrich <eric@oestrich.org>
2011-05-16 22:44:11 -0400

    non-plural hero seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
 Factory(:troop_type, :name => "Lord", :percentage => 25)
-Factory(:troop_type, :name => "Heroes", :percentage => 25)
+Factory(:troop_type, :name => "Hero", :percentage => 25)
 Factory(:troop_type, :name => "Core", :percentage => 50)
 Factory(:troop_type, :name => "Special", :percentage => 75)
 Factory(:troop_type, :name => "Rare", :percentage => 25)
